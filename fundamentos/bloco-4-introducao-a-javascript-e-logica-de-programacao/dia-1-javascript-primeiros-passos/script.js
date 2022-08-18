96824c021b395cab31a53acbf9cb20c169105077
Joey Hetfield <joeydefenix@hotmail.com>
2022-08-18 19:56:24 -0300

    Adicionando exercicio triangulo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -48,3 +48,15 @@ if (parametro > 0){
 }else if (parametro < 0){
     console.log('negative')
 }else console.log('zero')
+
+//🚀 Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+//Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+//Um ângulo será considerado inválido se não tiver um valor positivo.
+
+let angulo1 = 60;
+let angulo2 = 60;
+let angulo3 = 60;
+let triangulo = angulo1 + angulo2 + angulo3;
+if (triangulo === 180){
+    console.log(true)
+}else console.log(false)
