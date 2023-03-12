d27a8e3ea42e8d6427f969347370e733cc255c68
Joey Hetfield <joeydefenix@hotmail.com>
2023-03-12 14:04:10 -0300

    Ex 2

--- back-end/secao4/dia4/src/app.js
+++ back-end/secao4/dia4/src/app.js
@@ -5,6 +5,7 @@ const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateRating = require('./middlewares/validateRating');
 const validateDifficulty = require('./middlewares/validateDifficulty');
+const generateToken = require('./utils/generateToken');
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,15 @@ app.use(express.json());
 app.post('/activities', validateName, validatePrice, validateDescription, 
 validateCreatedAt, validateRating, validateDifficulty, (req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+  const token = generateToken();
+  return res.status(200).json({ token });
 });
 
 module.exports = app; 
